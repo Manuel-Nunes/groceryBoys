ae809aa7e4197d3d620df3dbb060c3ce039c1bb1
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-08-17 01:02:12 +0200

    Added auth routes and login. Still need register

---
+++ src/components/auth/AuthenticatedRoute.tsx
@@ -0,0 +1,16 @@
+import {Navigate, Route, RouteProps} from 'react-router-dom';
+import {useAuth} from '../../hooks/useAuth';
+
+interface Props {
+    children: JSX.Element
+}
+
+
+export default function AuthenticatedRoute({children}: Props) {
+    const auth = useAuth();
+    if (auth.user != null) {
+        return children;
+    } else {
+        return <Navigate to="/login" replace />;
+    }
+}
---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,157 @@
+import {Auth as CognitoAuth} from '@aws-amplify/auth';
+import {Hub, HubCallback} from '@aws-amplify/core';
+import {CognitoUser} from 'amazon-cognito-identity-js';
+import {createContext, ReactNode, useContext, useState} from 'react';
+import useAsyncEffect from 'use-async-effect';
+
+
+CognitoAuth.configure({
+  aws_project_region: 'us-east-1',
+  userPoolId: "us-east-1_bgjMgLW9i",
+  userPoolWebClientId: "2cp9jnng37l79i4v1lh7kgepim"
+});
+
+type User = {
+    email: string;
+}
+
+async function cognitoUserToUser(cognitoUser: CognitoUser): Promise<User> {
+    const attrList = await CognitoAuth.userAttributes(cognitoUser);
+    const attrs = Object.fromEntries(
+        attrList.map((attr) => [attr.getName(), attr.getValue()]),
+    );
+    console.log(JSON.stringify(attrs));
+    return {
+      email: attrs.email,
+    };
+}
+
+type Auth = ReturnType<typeof useProvideAuth>;
+const AuthContext = createContext<Auth | null>(null);
+
+export function AuthProvider({children}: { children: ReactNode }) {
+    const auth = useProvideAuth();
+    return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+    return useContext(AuthContext)!;
+}
+
+export type Credentials = Pick<User, 'email'> & {
+    password: string;
+};
+
+type CognitoUserWithChallenge = CognitoUser & {
+    challengeName: string | null | undefined;
+};
+
+function useProvideAuth() {
+    const [user, setUser] = useState<User | null>(null);
+    const [authInProgress, setAuthInProgress] = useState(true);
+  
+    /** Sign-in using email and password. */
+    async function logIn(
+        {
+          email,
+          password,
+        }: Credentials
+    ): Promise<User | string> {
+      const cognitoUser: CognitoUserWithChallenge = await CognitoAuth.signIn(email, password);
+      if (cognitoUser.challengeName) {
+        return cognitoUser.challengeName;
+      }
+      setUser(await cognitoUserToUser(cognitoUser));
+      setAuthInProgress(false);
+      return user!!;
+    }
+  
+    /**
+     * Log-out.
+     *
+     * @param global - if `true` the user will be logged out of all devices.
+     */
+    async function logOut(global = false) {
+      setUser(null);
+      await CognitoAuth.signOut({global});
+    }
+  
+    /**
+     * Sign-up using email, password, name and country.
+     *
+     * If we get back a confirmed user then we return a {@link User}, `null` otherwise.
+     */
+    async function signUp(
+        {
+          email,
+          password,
+        }: Credentials
+    ): Promise<User | null> {
+      const result = await CognitoAuth.signUp({
+        username: email,
+        password
+      });
+      if (result.user != null && result.userConfirmed) {
+        console.log(`signup successful: ${JSON.stringify(result.user)}`)
+        setUser(await cognitoUserToUser(result.user));
+        return user;
+      }
+      setUser(null);
+      return null;
+    }
+  
+    /**
+     * Loads the currently logged in {@link User}.
+     */
+    async function getUser() {
+      try {
+        const user: CognitoUserWithChallenge = await CognitoAuth.currentAuthenticatedUser();
+        if (!user.challengeName) {
+          setUser(await cognitoUserToUser(user));
+          setAuthInProgress(false);
+        }
+      } catch (e) {
+        setAuthInProgress(false);
+      }
+    }
+  
+    let listener: HubCallback;
+  
+    useAsyncEffect(
+        async (isMounted) => {
+          if (isMounted()) {
+            await getUser();
+            listener = async (data) => {
+              switch (data.payload.event) {
+                case 'signOut':
+                  setUser(null);
+                  setAuthInProgress(false);
+                  break;
+                case 'signIn_failure':
+                  setUser(null);
+                  setAuthInProgress(false);
+                  break;
+                case 'tokenRefresh_failure':
+                  setUser(null);
+                  setAuthInProgress(false);
+                  break;
+                default:
+              }
+            };
+  
+            Hub.listen('auth', listener);
+          }
+        },
+        () => listener != null && Hub.remove('auth', listener),
+        [],
+    );
+  
+    return {
+      user,
+      inProgress: authInProgress,
+      logIn,
+      logOut,
+      signUp,
+    };
+  }
+  
