9569a884c22f3dfe6efcb61b411ea62798747978
Manuel Nunes <122288176+Manuel-Nunes@users.noreply.github.com>
2023-08-19 12:14:55 +0200

    Move Routing and authentication to the index.jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {AuthProvider} from './hooks/useAuth';
 
-// import App from '@screen/App';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import AuthenticatedRoute from './components/auth/AuthenticatedRoute';
+import RegisterPage from './screens/Register';
+import LoginForm from './screens/Login';
 import App from './screens/App';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,6 +15,21 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route 
+          path="/"
+          element = {
+            <AuthenticatedRoute>
+              <App/>
+            </AuthenticatedRoute>
+          }>
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {AuthProvider} from '../hooks/useAuth';
-import AuthenticatedRoute from '../components/auth/AuthenticatedRoute';
-import LoginForm from './Login';
-import RegisterPage from './Register';
 
 function App() {
   return (
-      <AuthProvider>
-        <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/" element = {
-              <AuthenticatedRoute>
-              <HomePage/>
-            </AuthenticatedRoute>
-            }>
-          </Route>
-        </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+    <p>Hello World</p>
   );
-}
-
-function HomePage() {
-  return <><h2>Home</h2><p>home page</p></>;
 }
 
 export default App;
